12606ee6e5e53be9911c79b15711f8dfc0d80ee8
Mike Perkowitz <mike@perkowitz.net>
2021-01-02 18:19:18 -0800

    Copied issho repo into this repo. Removed new Hachi code, leaving original Hachi-for-launchpad.

--- src/main/java/net/perkowitz/issho/controller/midi/ChannelListener.java
+++
@@ -1,9 +0,0 @@
-package net.perkowitz.issho.controller.midi;
-
-public interface ChannelListener {
-
-    public void onNoteOn(int channel, int noteNumber, int velocity);
-    public void onNoteOff(int channel, int noteNumber, int velocity);
-    public void onCc(int channel, int ccNumber, int value);
-
-}
